bc3e0e1695cb2850d01aae8b3d46b8465a928809
ZoeVM9 <91099242+ZoeVM9@users.noreply.github.com>
2021-11-30 17:35:12 -0600

    More Activities / ListView

---
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto3/ConsultasActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_proyecto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consultas)
+    }
+}
---
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto3/EmpleadosActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_proyecto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmpleadosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empleados)
+    }
+}
